40d203cf86dcdbdaa5712e6da1c3819c64dbbfd3
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-08 11:50:27 +0300

    Video 22

--- BlazorTutorial/EmployeeManagement.Web/Services/IEmployeeService.cs
+++ BlazorTutorial/EmployeeManagement.Web/Services/IEmployeeService.cs
@@ -9,5 +9,6 @@ namespace EmployeeManagement.Web.Pages
     public interface IEmployeeService
     {
         Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int id);
     }
 }
