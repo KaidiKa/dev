0946da8fa7f71a91a3851c0f6656c86e4e7f77b4
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-08 12:58:37 +0300

    Video 27

--- BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -80,6 +80,7 @@ namespace EmployeeManagement.Web.Pages
         public IEmployeeService EmployeeService { get; set; }
 
         public IEnumerable<Employee> Employees { get; set; }
+        public bool ShowFooter { get; set; } = true;
 
         protected override async Task OnInitializedAsync()
         {
