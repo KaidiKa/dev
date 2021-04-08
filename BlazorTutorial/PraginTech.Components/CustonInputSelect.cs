23d6d625aa6393f17f64968ce6afd752f290d5f7
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-08 17:02:04 +0300

    Video 32

--- BlazorTutorial/PraginTech.Components/CustonInputSelect.cs
+++ BlazorTutorial/PraginTech.Components/CustonInputSelect.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
 
-namespace EmployeeManagement.Web.Pages
+namespace Pragin.Components
 {
     public class CustomInputSelect<TValue> : InputSelect<TValue>
     {
