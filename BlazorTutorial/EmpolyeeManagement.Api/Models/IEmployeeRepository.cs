2ce1ac96f06964dfefa5b6cf127f9ebe9bf7adb1
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-08 10:31:28 +0300

    Video 17

--- BlazorTutorial/EmpolyeeManagement.Api/Models/IEmployeeRepository.cs
+++ BlazorTutorial/EmpolyeeManagement.Api/Models/IEmployeeRepository.cs
@@ -12,6 +12,7 @@ namespace EmpolyeeManagement.Api.Models
         {
             Task<IEnumerable<Employee>> GetEmployees();
             Task<Employee> GetEmployee(int employeeId);
+            Task<Employee> GetEmployeeByEmail(string email);
             Task<Employee> AddEmployee(Employee employee);
             Task<Employee> UpdateEmployee(Employee employee);
             void DeleteEmployee(int employeeId);
