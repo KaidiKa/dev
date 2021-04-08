c8edce53069038fb6fe6a3d3ffd73cb8ac09095c
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-08 12:31:41 +0300

    Video 23

--- BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -5,12 +5,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeDetailsBase : ComponentBase
     {
         public Employee Employee { get; set; } = new Employee();
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
+
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
         [Parameter]
@@ -19,7 +39,9 @@ namespace EmployeeManagement.Web.Pages
         protected override async Task OnInitializedAsync()
         {
             Id = Id ?? "1";
-            Employee = await EmployeeService.GetEmployee(int.Parse(Id));        
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
+
     }
+
 }
