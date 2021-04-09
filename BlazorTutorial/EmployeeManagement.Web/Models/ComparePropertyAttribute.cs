08d618aa1fac7c713c36bb033890c17261f3e1de
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-09 23:30:06 +0300

    Video 37

---
+++ BlazorTutorial/EmployeeManagement.Web/Models/ComparePropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeManagement.Web.Models
+{
+    internal class ComparePropertyAttribute : Attribute
+    {
+        private string v;
+        private string errorMessage;
+
+        public ComparePropertyAttribute(string v, string ErrorMessage)
+        {
+            this.v = v;
+            errorMessage = ErrorMessage;
+        }
+    }
+}
---
+++ BlazorTutorial/EmployeeManagement.Web/Models/EmailDomainValidatorAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EmployeeManagement.Web.Models
+{
+    internal class EmailDomainValidatorAttribute : Attribute
+    {
+        public string AllowedDomain { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
