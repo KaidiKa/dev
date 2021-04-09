b109660f994f7a52806c1ef1b957ae967be2970e
KaidiKa <kaidi.kaldas@gmail.com>
2021-04-10 00:42:13 +0300

    Video 41

--- BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -12,56 +12,69 @@ namespace EmployeeManagement.Web.Pages
 {
     public class EditEmployeeBase : ComponentBase
     {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
         private Employee Employee { get; set; } = new Employee();
-        public EditEmployeeModel EditEmployeeModel { get; set; } = new EditEmployeeModel();
+
+        public EditEmployeeModel EditEmployeeModel { get; set; } =
+            new EditEmployeeModel();
 
         [Inject]
         public IDepartmentService DepartmentService { get; set; }
 
-        public List<Department> Departments { get; set; } = new List<Department>();
-
-        public string DepartmentId { get; set; }
-
-        [Inject]
-        public IEmployeeService EmployeeService { get; set; }
+        public List<Department> Departments { get; set; } = 
+            new List<Department>();
 
         [Parameter]
         public string Id { get; set; }
+
         [Inject]
         public IMapper Mapper { get; set; }
+
         [Inject]
         public NavigationManager NavigationManager { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
-            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+            int.TryParse(Id, out int employeeId);
+
+            if (employeeId != 0)
+            {
+                Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+            }
+            else
+            {
+                Employee = new Employee
+                {
+                    DepartmentId = 1,
+                    DateOfBrith = DateTime.Now,
+                    PhotoPath = "images/nophoto.jpg"
+                };
+            }
+
             Departments = (await DepartmentService.GetDepartments()).ToList();
-            DepartmentId = Employee.DepartmentId.ToString();
             Mapper.Map(Employee, EditEmployeeModel);
-
-            //EditEmployeeModel.EmployeeId = Employee.EmployeeId;
-            //EditEmployeeModel.FirstName = Employee.FirstName;
-            //EditEmployeeModel.LastName = Employee.LastName;
-            //EditEmployeeModel.Email = Employee.Email;
-            //EditEmployeeModel.ConfirmEmail = Employee.Email;
-            //EditEmployeeModel.DateOfBirth = Employee.DateOfBirth;
-            //EditEmployeeModel.Gender = Employee.Gender;
-            //EditEmployeeModel.PhotoPath = Employee.PhotoPath;
-            //EditEmployeeModel.DepartmentId = Employee.DepartmentId;
-            //EditEmployeeModel.Department = Employee.Department;
-            
-
         }
+
         protected async Task HandleValidSubmit()
         {
             Mapper.Map(EditEmployeeModel, Employee);
-            var result = await EmployeeService.UpdateEmployee(Employee);
-            
-            if(result != null)
+
+            Employee result = null;
+
+            if (Employee.EmployeeId != 0)
+            {
+                result = await EmployeeService.UpdateEmployee(Employee);
+            }
+            else
+            {
+                result = await EmployeeService.CreateEmployee(Employee);
+            }
+            if (result != null)
             {
                 NavigationManager.NavigateTo("/");
             }
-
         }
     }
 }
--- BlazorTutorial/EmployeeManagement.Web/Services/IEmployeeService.cs
+++ BlazorTutorial/EmployeeManagement.Web/Services/IEmployeeService.cs
@@ -11,5 +11,6 @@ namespace EmployeeManagement.Web.Pages
         Task<IEnumerable<Employee>> GetEmployees();
         Task<Employee> GetEmployee(int id);
         Task <Employee> UpdateEmployee(Employee updatedEmployee);
+        Task<Employee> CreateEmployee(Employee newEmployee);
     }
 }
